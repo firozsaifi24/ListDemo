3222b9d79185267f62b2498899b8670018c483cc
Firoz Saifi <firoz.saifi@algoworks.com>
2023-01-06 16:39:32 +0530

    Moved Main Activity to view package

--- app/src/main/java/com/firoz/listdemo/view/activity/MainActivity.kt
+++ app/src/main/java/com/firoz/listdemo/view/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.firoz.listdemo
+package com.firoz.listdemo.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.firoz.listdemo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
