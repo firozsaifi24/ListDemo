9fb1a373c2f1159f8873658ded76bf7bd0a6e517
Firoz Saifi <firoz.saifi@algoworks.com>
2023-01-07 18:32:02 +0530

    viewmodel created to call the api

---
+++ app/src/main/java/com/firoz/listdemo/viewmodel/MainViewModel.kt
@@ -0,0 +1,21 @@
+package com.firoz.listdemo.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.liveData
+import com.firoz.listdemo.api.ApiClient
+import com.firoz.listdemo.datastore.UsersDataSource
+import com.firoz.listdemo.model.UserDataResponse
+
+class MainViewModel : ViewModel() {
+
+    fun getUsersFlow(): LiveData<PagingData<UserDataResponse>> {
+        val config = PagingConfig(pageSize = 10, enablePlaceholders = true)
+        val datasource = UsersDataSource(ApiClient.request!!)
+        return Pager(config = config, pagingSourceFactory = { datasource }).liveData
+    }
+
+}
